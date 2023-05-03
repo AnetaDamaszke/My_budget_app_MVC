a2a2e71107ee743f9e03babc98142281757a4549
Aneta Damaszke <aneta.plaga@gmail.com>
2023-05-03 14:41:44 +0200

    Action filters added

--- App/Controllers/About.php
+++ App/Controllers/About.php
@@ -12,7 +12,7 @@ namespace App\Controllers;
     /**
      * Show the index page
      */
-    public function index()
+    protected function indexAction()
     {
         echo 'Hello from the index action in the About controller!';
         //echo '<p>Query string parameters: <pre>' .
@@ -22,7 +22,7 @@ namespace App\Controllers;
     /**
      * Show the add new page
      */
-    public function addNew()
+    protected function addNewAction()
     {
         echo 'Hello from the addNew action in the About controller!';
     }
@@ -30,7 +30,7 @@ namespace App\Controllers;
     /**
      * Show the edit page
      */
-    public function edit()
+    protected function editAction()
     {
         echo 'Hello from the edit action in the About controller!';
         echo '<p>Route parameters: <pre>' .
--- Core/Controller.php
+++ Core/Controller.php
@@ -22,4 +22,37 @@ namespace Core;
     {
         $this->route_params = $route_params;
     }
+
+    /**
+     * Magic method called when a non-existent or inaccessible method is
+     * called on an object of this class. Used to execute before and after
+     * filter methods on action methods.
+     */
+    public function __call($name, $args)
+    {
+        $method = $name . 'Action';
+
+        if (method_exists($this, $method)) {
+            if ($this->before() !== false) {
+                call_user_func_array([$this, $method], $args);
+                $this->after();
+            }
+        } else {
+            echo "Method $method not found in controller " . get_class($this);
+        }
+    }
+
+    /**
+     * Before filter - called before an action method.
+     */
+    protected function before() 
+    {
+    }
+
+    /**
+     * After filter - called after an action method.
+     */
+    protected function after() 
+    {     
+    }
 }
