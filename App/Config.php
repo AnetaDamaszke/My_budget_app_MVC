afa09642e8231b19dd07fcf364c3d1bd60e01f05
Aneta Damaszke <aneta.plaga@gmail.com>
2023-05-04 10:45:35 +0200

    Create config file

---
+++ App/Config.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+/**
+ * Application configuration
+ * 
+ * PHP versio 8.1.10
+ */
+
+ class Config
+ {
+    /**
+     * Database host
+     */
+    const DB_HOST = 'localhost';
+
+    /**
+     * Database name
+     */
+    const DB_NAME = 'mvc';
+
+    /**
+     * Database username
+     */
+    const DB_USER = 'root';
+
+    /**
+     * Database password
+     */
+    const DB_PASSWORD = '';
+ }
