fc416b446e452eb175668276428a4fe15afe6561
Aneta Damaszke <aneta.plaga@gmail.com>
2023-05-10 19:50:16 +0200

    Prevet session fixation attacks

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -30,6 +30,8 @@ use \App\Models\User;
 
         if ($user) {
 
+            session_regenerate_id(true);
+
             $_SESSION['user_id'] = $user->id;
 
             $this->redirect('/');
@@ -41,5 +43,30 @@ use \App\Models\User;
         }
     }
 
+    /**
+     * Log out a user
+     */
+    public function destroyAction()
+    {
+        $_SESSION = [];
+
+        if (ini_get('session.use_cookies')) {
+            $params = session_get_cookie_params();
+
+            setcookie(
+                session_name(),
+                '',
+                time() - 42000,
+                $params['path'],
+                $params['domain'],
+                $params['secure'],
+                $params['httponly']
+            );
+        }
+
+        session_destroy();
+
+        $this->redirect('/');
+    }
 
  } 
