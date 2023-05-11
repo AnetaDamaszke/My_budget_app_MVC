b311be4f38b4e664352cc7b07d3f535f2be3adc8
Aneta Damaszke <aneta.plaga@gmail.com>
2023-05-11 12:47:17 +0200

    Require login for all action methods in a controller

--- App/Controllers/Items.php
+++ App/Controllers/Items.php
@@ -13,14 +13,20 @@ use \App\Auth;
 
  class Items extends \Core\Controller
  {
+
+    /**
+     * Require to be authenticated before giving access to all methods in the cntroller
+     */
+    protected function before()
+    {
+        $this->requireLogin();
+    }
+
     /**
      * Item index
      */
-
     public function indexAction()
     {
-        $this->requireLogin();
-
         View::renderTemplate('Items/index.html');
     }
 
