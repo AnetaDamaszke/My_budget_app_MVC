ac2b111eb67a19065983486d282d4c2dd7fe073c
Aneta Damaszke <aneta.plaga@gmail.com>
2023-05-22 18:44:53 +0200

    Create and added incomes controller

---
+++ App/Controllers/Incomes.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+use \App\Auth;
+
+/**
+ * Incomes controller
+ * 
+ * PHP version 8.1.10
+ */
+
+class Incomes extends Authenticated
+{
+    /**
+     * Show the incomes page
+     */
+    public function indexAction()
+    {
+        View::renderTemplate('Incomes/index.html');
+    }
+}
